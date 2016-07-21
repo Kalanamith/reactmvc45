764f0340c13f95019d17bf80568cb58f42de55ba
Gayan Kalanamith <kalanamith@outlook.com>
2016-07-21 16:21:15 +0530

    Init

---
+++ ReactMVC45/ReactMVC45/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReactMVC45.Startup))]
+namespace ReactMVC45
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
